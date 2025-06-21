ddc942a204249c4567b064019206d6945bc0fa87
kazukk12 <k.kobayashi128@gmail.com>
2025-06-21 21:50:03 +0900

    add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
 import Header from "@/components/Header";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <div>こんにちは</div>
+    <main>
+      <Hero />
+    </main>
   );
 }
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+
+const Hero = () => {
+    return (
+        <section className="relative w-full h-screen">
+            {/* 背景画像 */}
+            <Image
+                src="/hero-bg.png"
+                alt="higway background" 
+                layout="fill"
+                objectFit="cover"
+                priority
+            />
+
+            {/* テキスト */}
+            <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center px-4">
+                <h1 className="text-4xl md:text-6xl font-bold drop-shadow-lg">
+                    Expand your options
+                </h1>
+                <h2 className="text-2xl md:text-4xl font-bold mt-2 drop-shadow-lg">
+                    from here.
+                </h2>
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
