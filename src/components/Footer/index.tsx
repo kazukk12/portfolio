177fcecff914263c0977a6f10ba4e9b306689a5f
kazukk12 <k.kobayashi128@gmail.com>
2025-06-21 16:15:09 +0900

    add Header

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return(
+        <footer>
+            <div>
+                <h1>My Portfolio</h1>
+                <nav>
+                    <a href="">Home</a>
+                    <a href="">Projects</a>
+                    <a href="">Contact</a>
+                </nav>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
