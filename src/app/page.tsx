a6a428211add0333dcf12a526a5e5acf67dd8149
kazukk12 <k.kobayashi128@gmail.com>
2025-06-22 16:56:41 +0900

    add works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 import About from "@/components/About";
+import Works from "@/components/Works";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Works />
     </main>
   );
 }
