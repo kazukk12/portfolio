4ab289a9b5b3a885e8f6d5c46fd68d73b2415575
kazukk12 <k.kobayashi128@gmail.com>
2025-06-22 16:31:55 +0900

    add about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
-import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   );
 }
