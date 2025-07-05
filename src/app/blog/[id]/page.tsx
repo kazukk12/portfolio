0ace702914a043c46e0c46f62a26aefc7e089551
kazukk12 <k.kobayashi128@gmail.com>
2025-07-05 22:42:49 +0900

    page.tsx

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,28 +1,26 @@
 import { getBlogDetail } from "@/lib/microcms";
 import { notFound } from "next/navigation";
 
-type Props = {
-    params: {
-        id: string;
-    };
-};
+export default async function BlogDetailPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const blog = await getBlogDetail(params.id);
 
-export default async function BlogDetailPage({params}: Props) {
-    const blog = await getBlogDetail(params.id);
+  if (!blog) {
+    notFound();
+  }
 
-    if (!blog) {
-        notFound(); //404リダイレクト
-    }
-
-    return (
-        <main className="p-6 max-w-3xl mx-auto">
-            <h1 className="text-3xl font-bold mb-4">{blog.title}</h1>
-            <div
-                className="prose" 
-                dangerouslySetInnerHTML={{
-                __html: blog.body,
-                }}
-            />
-        </main>
-    );
+  return (
+    <main className="p-6 max-w-3xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4">{blog.title}</h1>
+      <div
+        className="prose"
+        dangerouslySetInnerHTML={{
+          __html: blog.body,
+        }}
+      />
+    </main>
+  );
 }
