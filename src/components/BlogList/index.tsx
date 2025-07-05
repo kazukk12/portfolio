a8063a44f4e28bde9df36ca3284f894a0a373063
kazukk12 <k.kobayashi128@gmail.com>
2025-07-05 22:21:32 +0900

    add blog-detail

--- src/components/BlogList/index.tsx
+++ src/components/BlogList/index.tsx
@@ -3,6 +3,7 @@
 import { getBlogList } from "@/lib/microcms";
 import { Blog } from "@/types/microcms";
 import { useEffect, useState } from "react";
+import Link from "next/link";
 
 export default function BlogList() {
   const [blogs, setBlogs] = useState<Blog[]>([]);
@@ -16,17 +17,21 @@ export default function BlogList() {
 
   return (
     <section className="py-20 bg-gray-100">
-        <div className="max-w-5xl mx-auto px-4">
-            <h2 className="text-2xl font-bold mb-6">ブログ</h2>
-            <ul className="space-y-4">
-            {blogs.map((blog) => (
-                <li key={blog.id} className="border p-4 rounded shadow">
-                <h3 className="text-xl font-semibold">{blog.title}</h3>
-                <p className="text-sm text-gray-600">{blog.publishedAt}</p>
-                </li>
-            ))}
-            </ul>
-        </div>
+      <div className="max-w-5xl mx-auto px-4">
+        <h2 className="text-2xl font-bold mb-6">ブログ</h2>
+        <ul className="space-y-4">
+          {blogs.map((blog) => (
+            <li key={blog.id}>
+              <Link href={`/blog/${blog.id}`}>
+                <div className="border p-4 rounded shadow hover:bg-gray-200 transition cursor-pointer">
+                  <h3 className="text-xl font-semibold">{blog.title}</h3>
+                  <p className="text-sm text-gray-600">{blog.publishedAt}</p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   );
 }
